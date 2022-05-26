e93a40f40959f5f45c6d4e6663d8ab62aa60ca9e
aimunhidden485 <aimunnharr@gmail.com>
2022-05-26 11:02:24 +0600

    readme file edited

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Brush manufacturer
+## Live site link [https://manufacturer-website-ebeb5.web.app/]
+
+## Technologies used:
+* React
+* ES6
+* Tailwind
+* Daisy ui
+* React Router
+* Firebase
+* React query
+* React hook
+* React toastify
+* Mongodb
+* Node js
+
+## Features
+* It home, blogs, portfolio
+* Some private routes for admin
+* Login and signup page
+* You can add review in homepage
+* Admin can add product in product section
--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -26,14 +26,14 @@ const Navbar = () => {
             {
             user && <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
           }
-            <li tabindex="0">
-              <a class="justify-between">
-                Parent
-                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-              </a>
+            <li >
+            <NavLink to='/blogs'>
+              Blogs
+              
+            </NavLink>
 
-            </li>
-            <li><a>Item 3</a></li>
+          </li>
+            
           </ul>
         </div>
         <Link to='/' class="btn btn-ghost normal-case text-xl">Brush manufacturer</Link>
